439157252594da082d2a61f9eb5d5e31481c8afd
mohanlovewanshi <mlovewanshi@equalexperts.com>
2021-01-25 13:29:10 +0530

    Adding node app.js as startup file

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/",(req,res)=>{
      res.send("Welcome to the home page baby!!!!");
  });
-const port= process.env.port || 3000;
+const port= process.env.port || 5000;
 
 app.listen(port,()=>{
     console.log('App is up and running on Port : '+ port +"!!!!!")
